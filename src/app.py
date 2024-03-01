a29a2c68d0d7afe28786b99e74f46e22f7cc62db
rockdatio <info@rockdatio.com>
2024-03-01 00:12:18 -0500

    feat(jobs): adding jobs csv to database demo mysql

--- src/app.py
+++ src/app.py
@@ -9,9 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Eliam@localhost:33
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 class Department(db.Model):
     __tablename__ = 'departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+class Jobs(db.Model):
+    __tablename__ = 'jobs'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
 
@@ -20,13 +24,10 @@ def upload_csv_departments():
     csv_path = '../dataset/departments.csv'
     headers = ["id", "name"]
     df = pd.read_csv(csv_path, header=None, names=headers)
-    print(df.columns)
 
     for index, row in df.iterrows():
-        print(row)
         existing_department = Department.query.filter_by(id=row['id']).first()
         if existing_department is None:
-            # Create a new Department instance for each row
             new_department = Department(id=int(row['id']), name=row['name'])
             db.session.add(new_department)
 
@@ -35,6 +36,25 @@ def upload_csv_departments():
     return jsonify(
         {'success': True,
          'message': 'Departments uploaded successfully'})\
+        , 200
+
+@app.route('/upload-csv/jobs', methods=['POST'])
+def upload_csv_jobs():
+    csv_path = '../dataset/jobs.csv'
+    headers = ["id", "name"]
+    df = pd.read_csv(csv_path, header=None, names=headers)
+
+    for index, row in df.iterrows():
+        existing_job = Jobs.query.filter_by(id=row['id']).first()
+        if existing_job is None:
+            new_jobs = Jobs(id=int(row['id']), name=row['name'])
+            db.session.add(new_jobs)
+
+    # Commit the session to save all new departments to the database
+    db.session.commit()
+    return jsonify(
+        {'success': True,
+         'message': 'Jobs uploaded successfully'})\
         , 200
 
 # def upload_csv_to_db(csv_file):
